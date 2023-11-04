9a595c4dc3d8665e99bd367bfba0ac63287defa3
xsl <xsl.cmd@gmail.com>
2023-11-04 20:19:36 +0800

    fix

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,5 +1,5 @@
+CONFIG=$1
 srun -p s1_mm_research --gres=gpu:8 --ntasks=32 --ntasks-per-node=8 \
     --cpus-per-task=5 --kill-on-bad-exit=1 --quotatype=auto \
     python -u train_net.py \
-    --config-file  configs/coco/panoptic_segmentation/kmax_r50.yaml --launcher="slurm" \
-    OUTPUT_DIR 'output/kmax_r50_150k' 
+    --config-file  $CONFIG --launcher="slurm" 
